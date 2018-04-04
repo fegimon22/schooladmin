c2a490f74d32333b7cca6c8971602bc27544c35c
fegimon <fegimon@gmail.com>
2018-04-04 19:18:32 +0530

    school admin

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2018 <a href="#">Caltech Pvt ltd.</a>.</strong> All rights reserved.
+</footer>
